f65334557f9382f3479214b9d8469e0169713245
Oliver <oliveroenang@gmail.com>
2017-10-17 16:57:31 +0200

    autosuggest names added

--- static/js/script.js
+++ static/js/script.js
@@ -2,15 +2,17 @@
 // TO DO LIST / Wishlist
 // ========================================================
 
-document.querySelectorAll('.nav').forEach(function(ev) {
-  ev.addEventListener('click', function(el) {
-      smoothScroll(this, 15);
-      // el.preventDefault();
-  })
-});
+var alphabetNav = document.querySelectorAll('.nav');
 
-function smoothScroll(toElement, speed) {
-  var pointer = toElement.getAttribute('href').slice(1);
+for (var i = 0; i < alphabetNav.length; i++) {
+  alphabetNav[i].addEventListener('click', function(ev) {
+      smoothScroll(this, 15);
+      ev.preventDefault ? ev.preventDefault() : (ev.returnValue = false);
+  })
+};
+
+function smoothScroll(toLetter, speed) {
+  var pointer = toLetter.getAttribute('href').slice(1);
   var elem = document.getElementById(pointer);
   var elemOffset = elem.offsetTop; // returns the distance of the current element relative to the top of the offsetParent node
 
@@ -41,46 +43,93 @@ function smoothScroll(toElement, speed) {
 }
 
 function autocomplete() {
-  var people = []
-  var word = []
-  var contactList = document.querySelector('main');
-  var searchBox = document.querySelector('input[type=search]')
+  var people = [];
+  var word = [];
+  var mainElement = document.querySelector('main');
+  var contactList = mainElement.querySelectorAll('section')
+  var searchBox = document.querySelector('input[type=search]');
+
+  contactList.forEach(function (el) {
+    el.querySelectorAll('li').forEach(function(el) {
+      people.push(el.innerHTML.toLowerCase());
+    })
+  })
+
+  // Need some fixing doesnt return all names just the first ones in each
+  // for (var x = 0; x < contactList.length; x++) {
+  //   var list = contactList[x].querySelectorAll('li');
+    
+  //   for (var i = 0; i < list.length; list++) {
+  //     people.push(list[i].innerHTML.toLowerCase())
+  //   };
+  // };
+
   searchBox.addEventListener('input' , function(ev) {   
     var letter = word.push(ev.data);
 
     if (ev.data === null) {
       word.splice(word.length-2 , 2)
-    }
-
-    console.log(word.join('')); 
-
-    contactList.querySelectorAll('section').forEach(function (el) {
-      el.childNodes[3].querySelectorAll('li').forEach(function (el) {
-        people.push(el.innerHTML)
-      })
+    } 
+    
+    var searchQuery = word.join('');
+    
+    var filterPeople = people.filter( function(names) {
+      return names.includes(searchQuery);
     })
+
+    var autocomplete = document.querySelector('.autocomplete');
+    autocomplete.innerHTML= '';
+
+    filterPeople.forEach( function (name) {
+      autocomplete.innerHTML += '<li>' + name + '</li>';
+      autocomplete.classList.remove('hide')
+    })
+
+    // filterPeople.forEach(function(name) {
+    //   var contacts = document.querySelector('main');
+    //   // console.log('names' , name);
+    //   var filtered = document.querySelector('#letter-' + name.charAt(0));
+    //   // console.log(filtered.querySelector('h2').innerHTML)
+        
+    //   var newSection = document.createElement('section');
+    //   newSection.innerHTML = 
+    //     '<h2>' + filtered.querySelector('h2').innerHTML + '</h2>' +
+    //     '<li>' + filtered.querySelector('li').innerHTML + '</li>';
+
+    //   console.log(filtered);
+    //   // contacts.innerHTML = '';
+    //   contacts.appendChild(newSection);
+    // })
+
   })
-
-  // // collect all names n an array
-  // contactList.querySelectorAll('section').forEach(function (el) {
-  //   el.childNodes[3].querySelectorAll('li').forEach(function (el) {
-  //     people.push(el.innerHTML)
-  //   })
-  // })
-
 }
 
 autocomplete();
 
-// if (document.addEventListener) {
-//       document.querySelector('form').addEventListener('submit', add);
-//       document.querySelector('ul').addEventListener('click', remove);
-//       document.querySelector('.saveList').addEventListener('click', saveList);
-//       document.querySelector('.loadloaclstorage').addEventListener('click', show);
-// } else  {
-//       // !!! IE < 8 fallback for addeventlistener but queryselector doenst work so this is useless...
-//       document.querySelector('form').attachEvent('submit', add);
-//       document.querySelector('ul').attachEvent('click', remove);
-//       document.querySelector('.saveList').attachEvent('click', saveList);
-//       document.querySelector('.loadloaclstorage').attachEvent('click', show);
-// }
+// queryselector polyfill for IE!!
+if (!document.querySelectorAll) {
+  document.querySelectorAll = function (selectors) {
+    var style = document.createElement('style'), elements = [], element;
+    document.documentElement.firstChild.appendChild(style);
+    document._qsa = [];
+
+    style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
+    window.scrollBy(0, 0);
+    style.parentNode.removeChild(style);
+
+    while (document._qsa.length) {
+      element = document._qsa.shift();
+      element.style.removeAttribute('x-qsa');
+      elements.push(element);
+    }
+    document._qsa = null;
+    return elements;
+  };
+}
+
+if (!document.querySelector) {
+  document.querySelector = function (selectors) {
+    var elements = document.querySelectorAll(selectors);
+    return (elements.length) ? elements[0] : null;
+  };
+}
