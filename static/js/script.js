8c9e379b8e18734901740b8302ca529bce8cb49d
Oliver <oliveroenang@gmail.com>
2017-10-18 15:32:54 +0200

    styling + first letter of each section is shown

--- static/js/script.js
+++ static/js/script.js
@@ -2,15 +2,19 @@
 // TO DO LIST / Wishlist
 // ========================================================
 
+autocomplete();
+letterIndicator();
+
 var alphabetNav = document.querySelectorAll('.nav');
 
 for (var i = 0; i < alphabetNav.length; i++) {
   alphabetNav[i].addEventListener('click', function(ev) {
       smoothScroll(this, 15);
-      ev.preventDefault ? ev.preventDefault() : (ev.returnValue = false);
+      
   })
 };
 
+// Animate scroll to the name clicked from letter
 function smoothScroll(toLetter, speed) {
   var pointer = toLetter.getAttribute('href').slice(1);
   var elem = document.getElementById(pointer);
@@ -42,94 +46,92 @@ function smoothScroll(toLetter, speed) {
   }, 1);
 }
 
+// Store names on the page in to its own list
+function storeNames(people , mainElement) {
+  
+  var contactList = mainElement.querySelectorAll('li');
+
+  for (var x = 0; x < contactList.length; x++) {
+    var list = contactList[x].innerHTML;
+    people.push(list.toLowerCase())
+  };
+
+  return people;
+}
+
+// Upon typing show suggested names
 function autocomplete() {
-  var people = [];
   var word = [];
+  var people = [];
   var mainElement = document.querySelector('main');
-  var contactList = mainElement.querySelectorAll('section')
   var searchBox = document.querySelector('input[type=search]');
 
-  contactList.forEach(function (el) {
-    el.querySelectorAll('li').forEach(function(el) {
-      people.push(el.innerHTML.toLowerCase());
-    })
+  storeNames(people , mainElement);
+  
+  searchBox.addEventListener('input' , function(ev) {      
+      var searchQuery = searchBox.value;
+      var filterPeople = filterOutNames(searchBox.value , people);
+
+      renderSuggestions(searchBox , filterPeople , mainElement)
   })
+}
 
-  // Need some fixing doesnt return all names just the first ones in each
-  // for (var x = 0; x < contactList.length; x++) {
-  //   var list = contactList[x].querySelectorAll('li');
-    
-  //   for (var i = 0; i < list.length; list++) {
-  //     people.push(list[i].innerHTML.toLowerCase())
-  //   };
-  // };
+function renderSuggestions(searchBox , filterPeople , mainElement) {
+  var autocomplete = document.querySelector('.autocomplete-names');
+  var autocompleteSection = document.querySelector('.autocomplete');
+  autocomplete.innerHTML= '';
 
-  searchBox.addEventListener('input' , function(ev) {   
-    var letter = word.push(ev.data);
-
-    if (ev.data === null) {
-      word.splice(word.length-2 , 2)
-    } 
-    
-    var searchQuery = word.join('');
-    
-    var filterPeople = people.filter( function(names) {
-      return names.includes(searchQuery);
-    })
-
-    var autocomplete = document.querySelector('.autocomplete');
-    autocomplete.innerHTML= '';
+  if (searchBox.value.length > 0) {
+    var firstLetter = searchBox.value.charAt(0);
+    var h2FirsTLEtter = document.createElement('h2')
 
     filterPeople.forEach( function (name) {
       autocomplete.innerHTML += '<li>' + name + '</li>';
-      autocomplete.classList.remove('hide')
+      autocompleteSection.classList.remove('hide');
     })
 
-    // filterPeople.forEach(function(name) {
-    //   var contacts = document.querySelector('main');
-    //   // console.log('names' , name);
-    //   var filtered = document.querySelector('#letter-' + name.charAt(0));
-    //   // console.log(filtered.querySelector('h2').innerHTML)
-        
-    //   var newSection = document.createElement('section');
-    //   newSection.innerHTML = 
-    //     '<h2>' + filtered.querySelector('h2').innerHTML + '</h2>' +
-    //     '<li>' + filtered.querySelector('li').innerHTML + '</li>';
-
-    //   console.log(filtered);
-    //   // contacts.innerHTML = '';
-    //   contacts.appendChild(newSection);
-    // })
-
-  })
+  } else if (searchBox.value.length <= 0) {
+    autocompleteSection.classList.add('hide')
+  }
 }
 
-autocomplete();
+function filterOutNames(val , people) {
+  var peopleList = [];
 
-// queryselector polyfill for IE!!
-if (!document.querySelectorAll) {
-  document.querySelectorAll = function (selectors) {
-    var style = document.createElement('style'), elements = [], element;
-    document.documentElement.firstChild.appendChild(style);
-    document._qsa = [];
-
-    style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
-    window.scrollBy(0, 0);
-    style.parentNode.removeChild(style);
-
-    while (document._qsa.length) {
-      element = document._qsa.shift();
-      element.style.removeAttribute('x-qsa');
-      elements.push(element);
+  for (i = 0; i < people.length; i++) {
+    if (val === people[i].slice(0, val.length)) {
+      peopleList.push(people[i]);
     }
-    document._qsa = null;
-    return elements;
-  };
+  }
+
+  return peopleList;
 }
 
-if (!document.querySelector) {
-  document.querySelector = function (selectors) {
-    var elements = document.querySelectorAll(selectors);
-    return (elements.length) ? elements[0] : null;
-  };
+// Calculate the ofsett of an element 
+function offset(el) {
+  var rect = el.getBoundingClientRect();
+  return { top: rect.top }
+}
+
+// Check which name section is at the top of the page
+function letterIndicator() {
+  var sectionBegin = document.querySelectorAll('main section h2');
+  var sideNav = document.querySelector('aside');
+
+  window.addEventListener('scroll', function(e) {        
+    sectionBegin.forEach(function (el) {
+        var divOffset = offset(el);
+        var currentLetter = el.innerHTML;
+        // console.log(el , divOffset.top);
+        if (divOffset.top > -100 && divOffset.top <= 50) {
+          // console.log(el , 'IS BOVEN');
+          
+          var letterNav = document.querySelector('aside #nav-' + currentLetter);
+          letterNav.classList.add('big');                   
+        }  else {
+          var letterNav = document.querySelector('aside #nav-' + currentLetter);
+          letterNav.classList.remove('big');   
+        }
+      })
+  })
 }
